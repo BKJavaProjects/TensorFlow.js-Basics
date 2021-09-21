d66b0d91a5e01a235b20fb8e4703187a2607f0bd
Bennie Kowalski <65255810+BKJavaProjects@users.noreply.github.com>
2021-09-21 11:41:12 -0400

    Basic-Static-Server

--- Project 1/local-server/server.js
+++ Project 1/local-server/server.js
@@ -3,12 +3,11 @@ let app = express();
 
 app.use(function(req,res,next){
     console.log(`${new Date()} - ${req.method} request for ${req.url}`);
-    next(); // pass control to the next handler
+    next(); //pass control to the next handler
 });
 
 app.use(express.static("../static"));
 
-app.listen(81, function() {
-    console.log("Serving static on 81");
-    console.log('type in Firefox - localhost:81');
+app.listen(2000, function(){
+    console.log("Serving static on 2,000")
 });
